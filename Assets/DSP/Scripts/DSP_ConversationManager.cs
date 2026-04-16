a657a4d41a6e21eca3c7fc79e37f714482f24307
Y4IR0 <yairo_k@proton.me>
2026-04-16 15:55:18 +0200

    Started working on UI, created example UI.

---
+++ Assets/DSP/Scripts/DSP_ConversationManager.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class DSP_ConversationManager : MonoBehaviour
+{
+    public DSP_ConversationGraphAsset currentConversation;
+    public DSP_SettingsObject settings ;
+}
