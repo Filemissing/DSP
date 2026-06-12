e78e49335fcc1be764a35bf2d1f95600f0c5ba6f
Y4IR0 <yairo_k@proton.me>
2026-06-12 12:33:09 +0200

    Changed for easier integration

--- Assets/DSP/Scripts/DSP_EditorWindow.cs
+++ Assets/DSP/Scripts/DSP_EditorWindow.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Unity.Android.Gradle;
 using UnityEditor;
 using UnityEditor.Experimental.GraphView;
 using UnityEditor.UIElements;
